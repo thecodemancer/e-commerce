03daf1fadc2e6470a9d3a179728b4276779a562d
David Regalado <davidregalado12@hotmail.com>
2023-09-18 05:51:46 +0000

    changes

--- 1_batch/src/processors/functions.py
+++ 1_batch/src/processors/functions.py
@@ -11,14 +11,66 @@ log.setLevel(level=logging.INFO)
 def debug(text):
     log.info(f"debug:{text}")
 
-def filter(dataset, element){
+
+# Define a function to parse the CSV rows into a dictionary format
+def list_of_orders_parse(row):
+    return {
+        'order_id': row[0],
+        'order_date': row[1],
+        'customer_name': row[2],
+        'state': row[3],
+        'city': row[4]
+    }
+
+def sales_target_parse(row):
+    return {
+        'month_of_order_date': row[0],
+        'category': row[1],
+        'target': float(row[2])
+    }
+
+def order_details_parse(row):
+    return {
+        'order_id': row[0],
+        'amount': float(row[1]),
+        'profit': float(row[2]),
+        'quantity': int(row[3]),
+        'category': row[4],
+        'sub_category': row[5]
+    }
+
+def filter(dataset, element):
     if dataset=='sales_target':
-        pass
+        if (len(element['month_of_order_date']) == 0 or 
+            len(element['category']) == 0 or
+            len(element['target']) == 0
+            ) :
+            yield pvalue.TaggedOutput('sales_target_null', element)
+        else:
+            yield pvalue.TaggedOutput('sales_target_not_null', element)
     if dataset=='list_of_orders':
-        pass
+        if (len(element['order_id']) == 0 or 
+            len(element['order_date']) == 0 or
+            len(element['customer_name']) == 0 or
+            len(element['state']) == 0 or
+            len(element['city']) == 0
+            ) :
+            yield pvalue.TaggedOutput('list_of_orders_null', element)
+        else:
+            yield pvalue.TaggedOutput('list_of_orders_not_null', element)
+
     if dataset=='order_details':
-        pass
-}
+        if len(element['order_id']) == 0
+        if (len(element['amount']) == 0 or 
+            len(element['profit']) == 0 or
+            len(element['quantity']) == 0 or
+            len(element['category']) == 0 or
+            len(element['sub_category']) == 0
+            ) :
+            yield pvalue.TaggedOutput('order_details_null', element)
+        else:
+            yield pvalue.TaggedOutput('order_details_not_null', element)
+
 def split_talent_collection(element):
     for i, experience in enumerate(element['profiles'][0]['experience']):
         if experience.get('title'):
